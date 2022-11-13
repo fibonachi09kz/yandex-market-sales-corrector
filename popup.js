db3a75317aa01a7a55eb9b36798866b9ed8101a4
Danila Aleksin <danya09kz@gmail.com>
2022-11-14 03:50:11 +0600

    Summary update (based without async operations)

--- popup.js
+++ popup.js
@@ -85,7 +85,7 @@ function promoCalc() {
 
 	function retry() {
 		counter++
-		if (counter % 2 == 0) {
+		if (counter % 2 === 0) {
 			auto()
 		}
 	}
@@ -97,10 +97,10 @@ function promoCalc() {
 
 
 	document.addEventListener('keydown', (e) => {
-		if ( e.key == 'ArrowDown' ) {
+		if ( e.key === 'ArrowDown' ) {
 			observer.disconnect()
 			alert('Автоматизация завершена')
-		} else if ( e.key == 'ArrowUp' ) {
+		} else if ( e.key === 'ArrowUp' ) {
 			observer.observe(data, {childList: false, subtree: false, characterData: true, attributes: true});
 			alert('Автоматизация включена! Нажмите кнопку выделения всех товаров!')
 		}
@@ -109,133 +109,153 @@ function promoCalc() {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function saleCalc() {
 
-    // Получаем кнопку отметить всё
-    let checkAll = document.querySelector('table thead input[type="checkbox"]');
 
-
-	// Следим за нажатием по кнопке отметить всё
-	checkAll.addEventListener('change', function() {
-		// Получаем все строки таблицы
-		let tableRows = document.querySelectorAll('table tbody tr');
-		// Запускаем главную функцию перебора
-		fd(tableRows)
-	})
-
-
+	
 	const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
-
+	const nativeCheckboxValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "checked").set;
 	const delay = (n) => {
 		return new Promise (res => {
 			let tm = setTimeout (() => res(tm), n);
 		})
 	}
+	const ev = new Event('input', { bubbles: true});
 
-	function firstInputChange(inputF, price) {
-		nativeInputValueSetter.call(inputF, price);
-		let ev = new Event('input', { bubbles: true});
-		inputF.dispatchEvent(ev);
+	function nativeClick(elem) {
+		elem.dispatchEvent(new Event('click', { bubbles: true }));
+	}
+
+	function nativeDetect(event, elem) {
+		elem.dispatchEvent(ev);
 		let focus = new FocusEvent('focus')
 		let blur = new FocusEvent('blur')
-		inputF.dispatchEvent(focus);
-		inputF.dispatchEvent(blur);
+		elem.dispatchEvent(focus);
+		elem.dispatchEvent(blur);
+	}
+	function firstInputChange(inputF, price) {
+		nativeInputValueSetter.call(inputF, price);
+		nativeDetect(ev, inputF)
 	}
 	function zeroSetter(inputS) {
 		nativeInputValueSetter.call(inputS, '');
-		let ev = new Event('input', { bubbles: true});
-		inputS.dispatchEvent(ev);
-		let focus = new FocusEvent('focus')
-		let blur = new FocusEvent('blur')
-		inputS.dispatchEvent(focus);
-		inputS.dispatchEvent(blur);
+		nativeDetect(ev, inputS)
 	}
+
+
+
+	function checkboxSetter(checkboxElem, status) {
+		nativeCheckboxValueSetter.call(checkboxElem, status);
+		checkboxElem.dispatchEvent(new Event('click', { bubbles: true }));
+	}
+
+
+	const tableRows = document.querySelectorAll('table tbody tr');
+	const checkAllBtn = document.querySelector('table thead [type="checkbox"]');
         
-	const fd = async (tableRs) => {
+	const fd = (tableRs) => {
 
 		for ( const row of tableRs) {
 
-			row.style.backgroundColor = '#d4e3fa';
+			/////////////////////////////////////////////////////////////////////
+			let checkbox = row.querySelector('[type="checkbox"]'),
+			inputFirst = row.querySelectorAll('td')[6].querySelector('input'),
+			inputSecond = row.querySelectorAll('td')[7].querySelector('input'),
+			price = row.querySelectorAll('td')[5].querySelector('span.___Tag___xFCxD.__use--kind___TqC3g.__use--kind_tableBody____vp1n'),
+			sale = row.querySelectorAll('td')[8].querySelector('span.___Tag___xFCxD.__use--kind___TqC3g.__use--kind_tableBody____vp1n');
+			/////////////////////////////////////////////////////////////////////
 
-			let checkbox = row.querySelector('[type="checkbox"]'), //Получаем галочку
-				inputFirst = row.querySelectorAll('td')[6].querySelector('input'), //Получаем первое поле ввода в строке
-				inputSecond = row.querySelectorAll('td')[7].querySelector('input'), //Получаем второе поле ввода в строке
-				price = row.querySelectorAll('td')[5].querySelector('span.___Tag___xFCxD.__use--kind___TqC3g.__use--kind_tableBody____vp1n'), //Получаем основную цену
-				sale = row.querySelectorAll('td')[8].querySelector('span.___Tag___xFCxD.__use--kind___TqC3g.__use--kind_tableBody____vp1n'); //Получаем процент скидки
-			
+
+
+			// Запускаем обсервер для первого инпута
+			/////////////////////////////////////////////////////////////////////
+			let inputFirstObserver = new MutationObserver((mutationsList) => {
+				if (inputFirst.value <= price) {
+					zeroSetter(inputSecond)
+				}
+			});
+			inputFirstObserver.observe(inputFirst, {
+				attributes: true,
+				characterData: true
+			});
+			/////////////////////////////////////////////////////////////////////
+
+
+
+			// Запускаем обсервер для второго инпута
+			/////////////////////////////////////////////////////////////////////
+			let inputSecondObserver = new MutationObserver((mutationsList) => {
+				
+			});
+			inputSecondObserver.observe(inputSecond, {
+				attributes: true
+			});
+			/////////////////////////////////////////////////////////////////////
+
+
+
+			/////////////////////////////////////////////////////////////////////
 			let priceTxt = price.textContent;
 			priceTxt = priceTxt.replace(/\D/g,'');
-			let checkboxStatus = checkbox.checked
-			
-			firstInputChange(inputFirst, priceTxt);
-			await delay(200);
-			zeroSetter(inputSecond)
-			await delay(200);
-			
 			let saleTxt = sale.textContent;
 			saleTxt = saleTxt.replace(/\D/g,'');
+			let checkboxStatus = checkbox.checked
 
-			if (saleTxt == 0) {
-				row.style.backgroundColor = "#fad5d4"
-				if (checkboxStatus) {
-					checkbox.click()
-				}
-			} else {
-				row.style.backgroundColor = "#d4fae0"
+
+			if (checkboxStatus === false && saleTxt != 0) {
+				checkboxSetter(checkbox, true)
 			}
+			firstInputChange(inputFirst, priceTxt)
+			/////////////////////////////////////////////////////////////////////
+			
 			
 		}
-		let btn = document.querySelector('.__use--size___TJHkL.__use--size_s___vC9Zt .___Button___Ltx4B.__use--type___HP3kV.__use--type_page___a18Cq.__disabled___IQ_Hz');
+		let btn = document.querySelector('.__use--size___TJHkL.__use--size_s___vC9Zt .___Button___Ltx4B.__use--type___HP3kV.__use--type_page___a18Cq.__disabled___IQ_Hz').nextElementSibling;
 	
-		let nextPage = btn.nextElementSibling;
-	
-		let pageCon = nextPage.querySelector('span span')?.textContent
+		let pageCon = btn.querySelector('span span')?.textContent
 
 		if (pageCon) {
-			nextPage.click()
+			nativeClick(btn)
 		} else {
 			observer.disconnect()
 		}
 	}
 
-	checkAll.click()
-
-	let counter = 0
+	checkAllBtn.addEventListener('click', function() {
+		fd(tableRows)
+	})
 
 	let data = document.querySelector('[data-e2e="table-preloader"]')
 
 	let observer = new MutationObserver((mutationsList) => {
 		for (let mutation of mutationsList) {
             if (mutation.type === 'attributes') {
-				retry()
+				fd(tableRows)
 				break
 			}
         }
 	});
-
 	
 	observer.observe(data, {childList: false, subtree: false, characterData: true, attributes: true});
 
-	function retry() {
-		counter++
-		if (counter % 2 == 0) {
-			auto()
-		}
-	}
-
-	function auto() {
-		checkAll.click()
-	}
-
-
-	document.addEventListener('keydown', (e) => {
-		if ( e.key == 'ArrowDown' ) {
-			observer.disconnect()
-			alert('Автоматизация завершена')
-		} else if ( e.key == 'ArrowUp' ) {
-			observer.observe(data, {childList: false, subtree: false, characterData: true, attributes: true});
-			alert('Автоматизация включена! Нажмите кнопку выделения всех товаров!')
-		}
-	})
+	fd(tableRows)
     
 }
